722ecf72798b02569ea4cc8e9fbf36f91ab4ff5e
demiandinizdd <demian.dinizdd@campus.mccd.edu>
2021-02-02 15:46:30 -0300

    Fix: login layout and, routes

--- front-web/src/pages/Home/components/Login/index.tsx
+++ front-web/src/pages/Home/components/Login/index.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { makeLogin } from 'core/utils/request';
 import { saveSessionData } from 'core/utils/auth';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import ButtonIcon from 'core/components/ButtonIcon';
 import history from 'core/utils/history';
 import './styles.scss';
@@ -10,6 +14,8 @@ type FormState = {
     username: string;
     password: string;
 };
+
+config.autoA11y = true;
 
 const Login = () => {
     const { register, handleSubmit, errors } = useForm<FormState>();
@@ -27,6 +33,11 @@ const Login = () => {
         });
     }
 
+    const [showEye, setShowEye] = useState(false);
+    const togglePasswordVisiblity = () => {
+        setShowEye(showEye ? false : true);
+    };
+    
     return (
         <div className="login-content card-base">
             <h1 className="login-title">login</h1>
@@ -36,10 +47,10 @@ const Login = () => {
                 </div>
             )}
             <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                <div className="margin-bottom-30">
+                <div className="d-flex justify-content-center margin-bottom-30">
                     <input
                         type="email"
-                        className={`form-control input-base ${errors.username ? 'is-invalid' : ''}`}
+                        className={`d-flex input-base input-email w-100 justify-content-center ${errors.username ? 'is-invalid' : ''}`}
                         placeholder="Email"
                         name="username"
                         ref={register({
@@ -51,26 +62,33 @@ const Login = () => {
                         })}
                     />
                     {errors.username && (
-                        <div className="invalid-feedback d-block">
+                        <div className="d-block input-invalid">
                             {errors.username.message}
                         </div>
                     )}
                 </div>
                 <div>
-                    <input
-                        type="password"
-                        className={`form-control input-base ${errors.password ? 'is-invalid' : ''}`}
-                        placeholder="Senha"
-                        name="password"
-                        ref={register({ required: "Campo obrigatório" })}
-                    />
-                    {errors.password && (
-                        <div className="invalid-feedback d-block">
-                            {errors.password.message}
-                        </div>
-                    )}
+                    <div className="input-password-container">
+                        <input
+                            type={showEye ? "text" : "password"}
+                            placeholder="Senha"
+                            className={`d-flex input-base input-password w-100 ${errors.password ? 'is-invalid' : ''}`}
+                            name="password"
+                            ref={register({ required: "Campo Obrigatório"})}
+                        />
+                        <Link to="#" className="hide-show-password d-flex" onClick={togglePasswordVisiblity}>
+                            {showEye ? <FontAwesomeIcon className="d-flex align-self-center " icon={faEyeSlash} /> : <FontAwesomeIcon className="d-flex align-self-center" icon={faEye}/>}
+                        </Link>
+                    </div>
+                        {
+                            errors.password && (
+                                <div className="d-block input-invalid">
+                                        {errors.password.message}
+                                </div>
+                            )
+                        }
                 </div>
-                <div className="login-button">
+                <div className="login-button d-flex">
                     <ButtonIcon text="logar" />
                 </div>
             </form>
