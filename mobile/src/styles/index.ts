0e987bf30747c49594d062bc7ec50ef5cb4b8fb3
demiandinizdd <demian.dinizdd@campus.mccd.edu>
2021-02-28 17:06:11 -0300

    Add colors from figma prototype

---
+++ mobile/src/styles/index.ts
@@ -0,0 +1,13 @@
+const colors = {
+    white: "#FFF",
+    black: "#000",
+    primary: "#FFC700",
+    secondary: "#937d1c",
+    title: "#F2F2F2",
+    subTitle: "#9E9E9E",
+    borderColor: "#E1E1E1",
+    darkGray: "#525252",
+    gray: "#6C6C6C",
+    mediumGray: "#BFBFBF",
+    lightGray: "#CDCDCD"
+};
